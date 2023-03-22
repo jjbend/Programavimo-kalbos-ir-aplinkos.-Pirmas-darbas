f39e1319d76f6898f7e3bf40b6144967dea174dd
jjbend <madavenueinfo@gmail.com>
2023-03-22 16:06:31 +0200

    pakeitimai

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ app = Flask(__name__,static_url_path='')
 variable=0
 array = []
 
+connection=sqlite3.connect(".\\NotesDatabase.db")
+#connection=sqlite3.connect("C:\Users\1538822\REPOS\darbas")
+
 @app.route("/")
 def mano_funkcija():
     return ("Labas")
@@ -27,31 +30,65 @@ def notes():
     if(request.method == "POST"):
         global array
         args=request.form.get("note2")
+        array_2=select_from_db()
         if (args):
             array.append(args)
+            insert_into_db(args)
             print(array)
-        return render_template('./notes.html', note=array)
+        return render_template('./notes.html', note=select_from_db())
     else:
-        
-       return render_template('./notes.html',note=array)
+       array_2=select_from_db()
+       return render_template('./notes.html',note=select_from_db())
 def add_to_array():
     array = []
 
 
-    def createDB():
-        connection=sqlite3.connect("C:\\Users\\1538822\\Desctop\\NotesDatabase.db")
-        cursor=connection.cursor()
-
-        createTableString= """CREATE TABLE IF NOT EXISTS Sheets (
-            Id INTEGER PRIMARY KEY AUTOINCREMENT,
-            Name TEXT NOT NULL
-        )"""
+def createDB():
+    connection=sqlite3.connect(".\\NotesDatabase.db")
+    cursor=connection.cursor()
         
-        cursor.execute(createTableString)
+    createTableString = """ CREATE TABLE IF NOT EXISTS Sheets (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Name TEXT NOT NULL
+    )
+    
+    """
+    createNotesTableString = """ CREATE TABLE IF NOT EXISTS Notes (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        SheetId INTEGER NOT NULL,
+        Header TEXT,
+        Text TEXT,
+        FOREIGN KEY (SheetId) REFERENCES Sheets(Id)
+    )
+    
+    
+    """
+    
+    cursor.execute(createTableString)
+    cursor.execute(createNotesTableString)
+
+def insert_into_db(note):
+    conn=sqlite3.connect(".\\NotesDatabase.db")
+    queryString="""
+        INSERT INTO Sheets (Name) Values (?)
+    """
+
+    cur=conn.cursor()
+    cur.execute(queryString,(note,))
+    conn.commit()
+
+def select_from_db():
+    conn=sqlite3.connect(".\\NotesDatabase.db")
+    queryString="""
+        SELECT name FROM Sheets
+    """
+    cur=conn.cursor()
+    array = cur.execute(queryString).fetchall()
+    return array
 
 if __name__ =="__main__":
-    
-    app.run(host='0.0.0.0', port=5000, debug="true")
+    createDB()
+    app.run(debug="true")
 
 
 
